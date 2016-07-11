e23772cfdbb3ffeb6ca39f8a4d9719e773a79f29
Matthew D. Huff <matthewhuff89@gmail.com>
2016-07-11 14:33:53 -0400

    Gratitude section, starting to add jquery stuff

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("get you some");
+$(document).ready(function(){
+  console.log("get you some");
+
+  $("#gratitude-button").click(function(e){
+    e.preventDefault();
+    $('#purpose').fadeOut(function(){
+      $('#gratitude').fadeIn();
+    });
+  })
+
+
+
+});
